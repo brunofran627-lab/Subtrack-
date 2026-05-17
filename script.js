a28daee463ce318032afc6add13a69f72daa0992
brunofran627-lab <brunofran627@gmail.com>
2026-05-17 15:28:07 +0000

    Update script.js
    Atualização de saas

--- script.js
+++ script.js
@@ -1,66 +1,26 @@
+ let subs = [];
 
-  body {
-  margin: 0;
-  font-family: Arial;
-  background: #0a0f1c;
-  color: white;
+function addSub() {
+  const name = document.getElementById("name").value;
+  const price = parseFloat(document.getElementById("price").value);
+
+  if (!name || isNaN(price)) return;
+
+  subs.push({ name, price });
+
+  render();
 }
 
-/* HEADER */
-header {
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 40px;
-  background: #111827;
-}
+function render() {
+  const list = document.getElementById("list");
+  let total = 0;
 
-.logo {
-  font-weight: bold;
-}
+  list.innerHTML = "";
 
-/* HERO */
-.hero {
-  text-align: center;
-  padding: 80px 20px;
-}
+  subs.forEach(s => {
+    total += s.price;
+    list.innerHTML += `<p>${s.name} - €${s.price.toFixed(2)}</p>`;
+  });
 
-.hero h1 {
-  font-size: 42px;
-}
-
-.hero p {
-  color: #aaa;
-}
-
-/* APP */
-.app {
-  text-align: center;
-}
-
-.card {
-  background: #111827;
-  width: 320px;
-  margin: auto;
-  padding: 20px;
-  border-radius: 12px;
-}
-
-input {
-  width: 100%;
-  padding: 10px;
-  margin: 5px 0;
-}
-
-button {
-  width: 100%;
-  padding: 10px;
-  background: #22c55e;
-  border: none;
-  border-radius: 6px;
-  font-weight: bold;
-  cursor: pointer;
-}
-
-#list {
-  margin-top: 20px;
-}
+  document.getElementById("total").innerText =
+    "Total: €" + total.toFixed(2);
