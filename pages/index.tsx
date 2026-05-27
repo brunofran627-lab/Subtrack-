1915d50d1052219cf2614f4e316a2daae0483ab3
brunofran627-lab <brunofran627@gmail.com>
2026-05-27 11:14:40 +0000

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Subtrack</h1>
-      <p>Welcome to your SaaS</p>
-    </main>
-  );
-}
--- pages/pages/login.tsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <main>
-      <h1>Login</h1>
-      <p>Sign in to continue</p>
-    </main>
-  );
-}
