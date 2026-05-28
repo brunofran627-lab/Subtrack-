fba5b1163d34070ad7b4fe21b43d3ddb602e817f
brunofran627-lab <brunofran627@gmail.com>
2026-05-28 05:26:42 +0000

    Update prisma.ts
    Resolver erro

--- prisma/prisma/lib/prisma.ts
+++ prisma/prisma/lib/prisma.ts
@@ -1,1 +1,14 @@
 import { prisma } from '../../lib/prisma'
+imp
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
